d658556f7b3b7936640de498df6d9221fb11d6c2
sunzk <312797831@qq.com>
2021-07-28 05:00:23 +0800

    处理意外导致的代码不同步QAQ

--- base/src/main/java/com/sunzk/base/network/BaseService.kt
+++ base/src/main/java/com/sunzk/base/network/BaseService.kt
@@ -1,14 +1,12 @@
 package com.sunzk.demo.service
 
-import android.util.Log
+import com.sunzk.base.network.waitResponse
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.withContext
-import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.Response
-import java.util.concurrent.TimeUnit
 
 class BaseService(init: ((clientBuilder: OkHttpClient.Builder) -> Unit?)? = null) {
 
